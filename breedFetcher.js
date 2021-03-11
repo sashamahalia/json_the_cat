cf39eca8d4fcbf31d0a2e6c342ecc4e6782d2842
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-11 23:33:51 +0000

    parceled code into modules, and created testing using mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-// console.log(args);
 
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log('Error:', error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`${breedName} is not a valid breed`);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`${breedName} is not a valid breed`);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,27 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns null for an ivalid breed, via callback', (done) => {
+    fetchBreedDescription('Siberiax', (err, desc) => {
+      assert.equal(desc, null);
+
+      const expectedDesc = null;
+
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+});
